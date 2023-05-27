5d87690d93d6f9cadcd9deabc06887d3d082d020
dmmaks <54597096+dmmaks@users.noreply.github.com>
2023-05-27 19:39:47 +0300

    change of path

--- dilpoma-backend/src/main/java/com/edu/netc/bakensweets/controller/TechniqueMitigationController.java
+++ dilpoma-backend/src/main/java/com/edu/netc/bakensweets/controller/TechniqueMitigationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Validated
-@RequestMapping("/api/techniques-mitigations")
+@RequestMapping("/api/techniquesMitigations")
 public class TechniqueMitigationController {
     private final TechniqueMitigationService techniqueMitigationService;
 
